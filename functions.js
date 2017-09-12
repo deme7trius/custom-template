efe2772b3dd26ddcf66278a070ee2bde81d1a30e
deme7trius <31722521+deme7trius@users.noreply.github.com>
2017-09-12 17:59:22 +0300

    finished job for the day
    finished job for the day

---
+++ functions.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+//flexslider
+
+        $('.flexslider').flexslider({
+            animation: "slide",
+            slideshowSpeed: 10000,
+            animationSpeed: 4000,
+            touch: true,
+            easing: "swing",
+            animationLoop: true,
+            directionNav: true
+        });
+      //end function
+});
