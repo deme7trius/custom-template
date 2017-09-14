d53697a0a4d28643a53a2de69d36adface0e61ed
deme7trius <31722521+deme7trius@users.noreply.github.com>
2017-09-14 17:14:19 +0300

    sadas
    ads

--- functions.js
+++ functions.js
@@ -1,42 +1,57 @@
 $(document).ready(function () {
 
-    $( window ).scroll(function() {
-     var t = $('#Body');
-     $( "header" ).css( "padding", "0" );
-     $("div.layer").css( "display", "block" );
-     if(t.scrollTop() == 0)
-     {
-      $( "header" ).css( "padding", "20px 0" );
-      $("div.layer").css( "display", "none" )
-    }
+	$( window ).scroll(function() {
+		var t = $('#Body');
+		$( "header" ).css( "padding", "0" );
+		$("div.layer").css( "display", "block" );
+		if(t.scrollTop() == 0)
+		{
+			$( "header" ).css( "padding", "20px 0" );
+			$("div.layer").css( "display", "none" )
+		}
 
-  });
+	});
 //flexslider
 
-        $('#main').flexslider({
-            animation: "slide",
-            slideshowSpeed: 10000,
-            animationSpeed: 4000,
-            touch: true,
-            easing: "swing",
-            animationLoop: true,
-            directionNav: true
-        });
-      $('#secondary').flexslider({
-      	animation: "slider"
-      });
+  $('#main').flexslider({
+	    animation: "slide",
+	slideshowSpeed: 10000,
+	animationSpeed: 4000,
+	touch: true,
+	easing: "swing",
+	animationLoop: true,
+	directionNav: true
+  });
+$('#secondary').flexslider({
+	animation: "slider"
+});
       //end function
       $(window).scroll(function () {
-    $('#phoneimage').each(function () {
-        var imagePos = $(this).offset().top;
-        var imageHeight = $(this).height();
-        var topOfWindow = $(window).scrollTop();
+      	$('#phoneimage').each(function () {
+      		var imagePos = $(this).offset().top;
+      		var imageHeight = $(this).height();
+      		var topOfWindow = $(window).scrollTop();
 
-        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
-           $(this).css("background-attachment", "unset"); 
-        } else {
-            $(this).css("background-attachment", "fixed");
-        }
-    });
-});
-});
+      		if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+      			$(this).css("background-attachment", "unset"); 
+      		} else {
+      			$(this).css("background-attachment", "fixed");
+      		}
+      	});
+      });
+       //end function
+       $(".up").click(function(e){
+       	e.preventDefault();
+       	$('html, body').animate({ scrollTop: 0 }, 1000);
+       });
+
+       window.onscroll = function() {scrollFunction()};
+       $('.up').css('display','none');
+       function scrollFunction() {
+       	if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+       		$('.up').css('display','block');
+       	} else {
+       		$('.up').css('display','none');
+       	}
+       }
+   });
